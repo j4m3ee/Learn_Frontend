2fe85c9b49e598437393a774e74591fc7b805c47
j4m3ee <j4m3ee01@gmail.com>
2021-03-22 10:22:53 +0700

    loading todo and done

--- src/container/donePage.js
+++ src/container/donePage.js
@@ -3,15 +3,19 @@ import axios from 'axios'
 import Card from '../components/card'
 import Navbar from '../components/navbar'
 import { AuthApi } from '../AuthApi'
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 export default function DonePage() {
     const [selectedCard, setSelectedCard] = useState(null)
     const [tasks, setTasks] = useState([])
-    const {user} = useContext(AuthApi)
+    const { user } = useContext(AuthApi)
+    const [loading, setLoading] = useState(false)
 
     const fetchData = async () => {
+        setLoading(true)
         const res = await axios.get(`https://learn-backend-snapm.herokuapp.com/api/tasks?isFinished=true&user_id=${user._id}`)
         setTasks(res.data)
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -19,47 +23,54 @@ export default function DonePage() {
     }, [])
 
     const onUnDone = async (id) => {
-        try{
+        try {
             await axios
-            .put(`https://learn-backend-snapm.herokuapp.com/api/task?id=${id}`, {
-                isFinished: false
-            })
+                .put(`https://learn-backend-snapm.herokuapp.com/api/task?id=${id}`, {
+                    isFinished: false
+                })
 
             const newTasks = tasks.filter(ele => ele._id !== id)
             setTasks(newTasks)
-        } catch (err){
+        } catch (err) {
             console.log(err)
         }
     }
 
     const onDelete = async (id) => {
-        try{
+        try {
             await axios.delete(`https://learn-backend-snapm.herokuapp.com/api/task/${id}`)
 
             const newTasks = tasks.filter(ele => ele._id !== id)
             setTasks(newTasks)
-        }catch (err){
+        } catch (err) {
             console.log(err)
         }
     }
 
     return (
         <div className="TodoPage">
-            <Navbar/>
+            <Navbar />
             <h1>Done</h1>
-            <div className="card-container">
-                {tasks.length !== 0 ? tasks.map((ele, i) => (
-                    <Card
-                        mode={"done"}
-                        onUnDone={() => onUnDone(ele._id)}
-                        onDelete={() => onDelete(ele._id)}
-                        detail={ele}
-                        selected={selectedCard === i}
-                        key={i}
-                        onClick={()=> setSelectedCard(i)}
-                    />
-                )) : <p style={{ textAlign: "center" }}>You haven't done any task.</p>}
-            </div>
+            {loading ?
+                <div className="loading">
+                    <PropagateLoader color={'#272727'} loading={loading} size={15} />
+                </div>
+                :
+                <div className="card-container">
+                    {tasks.length !== 0 ? tasks.map((ele, i) => (
+                        <Card
+                            mode={"done"}
+                            onUnDone={() => onUnDone(ele._id)}
+                            onDelete={() => onDelete(ele._id)}
+                            detail={ele}
+                            selected={selectedCard === i}
+                            key={i}
+                            onClick={() => setSelectedCard(i)}
+                        />
+                    )) : <p style={{ textAlign: "center" }}>You haven't done any task.</p>}
+                </div>
+            }
+
         </div>
     )
 }
--- src/container/todoPage.js
+++ src/container/todoPage.js
@@ -4,16 +4,20 @@ import { useHistory } from 'react-router-dom'
 import Card from '../components/card'
 import Navbar from '../components/navbar'
 import { AuthApi } from '../AuthApi'
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 export default function TodoPage() {
     const history = useHistory()
     const [selectedCard, setSelectedCard] = useState(null)
     const [tasks, setTasks] = useState([])
-    const {user} = useContext(AuthApi)
+    const { user } = useContext(AuthApi)
+    const [loading, setLoading] = useState(false)
 
     const fetchData = async () => {
+        setLoading(true)
         const res = await axios.get(`https://learn-backend-snapm.herokuapp.com/api/tasks?isFinished=false&user_id=${user._id}`)
         setTasks(res.data)
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -25,47 +29,54 @@ export default function TodoPage() {
     }
 
     const onDone = async (id) => {
-        try{
+        try {
             await axios.put(`https://learn-backend-snapm.herokuapp.com/api/task?id=${id}`, {
                 isFinished: true
             })
 
             const newTasks = tasks.filter(ele => ele._id !== id)
             setTasks(newTasks)
-        } catch (err){
+        } catch (err) {
             console.log(err)
         }
     }
 
     const onDelete = async (id) => {
-        try{
+        try {
             await axios.delete(`https://learn-backend-snapm.herokuapp.com/api/task/${id}`)
 
             const newTasks = tasks.filter(ele => ele._id !== id)
             setTasks(newTasks)
-        }catch (err){
+        } catch (err) {
             console.log(err)
         }
     }
 
     return (
         <div className="TodoPage">
-            <Navbar/>
+            <Navbar />
             <h1>Todo</h1>
-            <div className="card-container">
-                {tasks.length !== 0 ? tasks.map((ele, i) => (
-                    <Card
-                        mode={"default"}
-                        onDone={() => onDone(ele._id)}
-                        onEdit={() => onEdit(ele._id)}
-                        onDelete={() => onDelete(ele._id)}
-                        detail={ele}
-                        selected={selectedCard === i}
-                        key={i}
-                        onClick={()=> setSelectedCard(i)}
-                    />
-                )) : <p style={{ textAlign: "center" }}>Hooray! You have no more task todo.</p>}
-            </div>
+            {loading ?
+                <div className="loading">
+                    <PropagateLoader color={'#272727'} loading={loading} size={15} />
+                </div>
+                :
+                <div className="card-container">
+                    {tasks.length !== 0 ? tasks.map((ele, i) => (
+                        <Card
+                            mode={"default"}
+                            onDone={() => onDone(ele._id)}
+                            onEdit={() => onEdit(ele._id)}
+                            onDelete={() => onDelete(ele._id)}
+                            detail={ele}
+                            selected={selectedCard === i}
+                            key={i}
+                            onClick={() => setSelectedCard(i)}
+                        />
+                    )) : <p style={{ textAlign: "center" }}>Hooray! You have no more task todo.</p>}
+                </div>
+            }
+
         </div>
     )
 }
