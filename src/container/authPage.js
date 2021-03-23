18649a89fb2528e531b70f23b7afbfef65540d61
j4m3ee <j4m3ee01@gmail.com>
2021-03-24 01:18:48 +0700

    loading auth

--- src/container/authPage.js
+++ src/container/authPage.js
@@ -4,6 +4,7 @@ import './index.css'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 import { AuthApi } from '../AuthApi'
+import HashLoader from "react-spinners/HashLoader"
 
 export default function AuthPage() {
     const [email, setEmail] = useState('')
@@ -12,6 +13,7 @@ export default function AuthPage() {
     const [hasAccount, setHasAccount] = useState(false)
     const { getUser } = useContext(AuthApi)
     const URL = 'https://learn-backend-snapm.herokuapp.com/api/'
+    const [loading,setLoading] = useState(false)
 
     const clearInput = () => {
         setEmail('')
@@ -22,11 +24,13 @@ export default function AuthPage() {
     const handleLogin = async () => {
         console.log("sign in", email)
         try {
+            setLoading(true)
             const res = await axios.post(`${URL}login`, {
                 userName: email,
                 password: password
             }).then((res) => {
                 if (res.data.auth) {
+                    setLoading(false)
                     console.log('Auth page')
                     console.log(res.data)
                     Cookies.set("token", res.data.token)
@@ -44,6 +48,7 @@ export default function AuthPage() {
             })
 
         } catch (err) {
+            setLoading(false)
             console.log(err)
             setError(err.message)
         }
@@ -52,11 +57,13 @@ export default function AuthPage() {
     const handleSignup = async () => {
         console.log("sign up", email)
         try {
+            setLoading(true)
             await axios.post(`${URL}user`, {
                 userName: email,
                 password: password
             }).then((result) => {
                 if (result.data.auth) {
+                    setLoading(false)
                     console.log(result.data)
                     Cookies.set("token", result.data.token)
                     window.location.reload();
@@ -72,6 +79,7 @@ export default function AuthPage() {
                 }
             })
         } catch (err) {
+            setLoading(false)
             console.log(err)
             setError(err.message)
         }
@@ -92,6 +100,10 @@ export default function AuthPage() {
                 error={error}
                 setError={setError}
             />
+            {loading ?
+        <div className="auth-loading">
+          <HashLoader className="auth-loading" color={'#272727'} loading={loading} size={100} />
+        </div> : <div> </div>  }
         </div>
     )
 }
