6fc1759fcbd4907c0b4d09326017c2e29d8acccd
j4m3ee <j4m3ee01@gmail.com>
2023-03-08 09:58:52 +0700

    Fix API-Endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "http://localhost:1000/api/";
+const API_ENDPOINT = "http://todona-api.surawit.com/api/";
 const SITE_KEY = "6LfCkawaAAAAAL4pUFgWI04jhrCavJAWC_v8_sqi";
 
 export { API_ENDPOINT, SITE_KEY };
