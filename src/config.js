1235a6f326425b691b12a4617b4b60ff0a259b48
j4m3ee <j4m3ee01@gmail.com>
2023-03-15 15:03:50 +0700

    test push

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 const API_ENDPOINT = "https://todona-api.surawit.com/api/";
-// const API_ENDPOINT = "http://localhost:1000/api/";
 const SITE_KEY = "6LfCkawaAAAAAL4pUFgWI04jhrCavJAWC_v8_sqi";
 
 export { API_ENDPOINT, SITE_KEY };
