9a42536fc9e6971a3db25df44965daab703e307c
Surawit Yosaeng <54217896+j4m3ee@users.noreply.github.com>
2024-07-09 10:56:46 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "https://todona-api.surawit.com/api/";
+const API_ENDPOINT = "https://learn-backend-gdlg.onrender.com/api/";
 const SITE_KEY = "6LfCkawaAAAAAL4pUFgWI04jhrCavJAWC_v8_sqi";
 
 export { API_ENDPOINT, SITE_KEY };
